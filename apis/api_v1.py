3c0e9fc45e9f1c4f85a4c302ed784d904bd6ca2c
Lucas Anderson <lucas.peter.anderson@gmail.com>
2025-08-16 09:51:51 -0400

    - fixed typo

--- apis/api_v1.py
+++ apis/api_v1.py
@@ -7,9 +7,9 @@ from apis.namespaces.v1.vehicles import api as vehicles_ns
 blueprint = Blueprint("v1", __name__, url_prefix="/api/v1")
 api = Api(
     blueprint,
-    title="Vehical Query Engine (VQE)",
+    title="Vehicle Query Engine (VQE)",
     version="1.0",
-    description="Welcome to the Vehical Query Engine (VQE) documentation site!",
+    description="Welcome to the Vehicle Query Engine (VQE) documentation site!",
 )
 
 api.add_namespace(vehicles_ns)
